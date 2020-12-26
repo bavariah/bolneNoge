d1337996f2e0e61aaf5c54192296f7a0d3e897eb
bavariah <48217024+bavariah@users.noreply.github.com>
2020-12-26 14:18:22 +0100

    f2

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $(".menu-toggle").click(function() {
-    $("nav").toggleClass("active");
+    $(".nav").toggleClass("active");
   });
 
   $("ul li").click(function() {
@@ -47,3 +47,23 @@ $(".post-wrapper").slick({
     // instead of a settings object
   ]
 });
+
+//Get the button
+var mybutton = document.getElementById("topBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 1200 || document.documentElement.scrollTop > 800) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
