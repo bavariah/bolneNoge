6bc1f2fb365de35566d311ff553bdbdb0be37ca6
bavariah <48217024+bavariah@users.noreply.github.com>
2020-12-27 12:36:20 +0100

    f1

---
+++ scripts/app.js
@@ -0,0 +1,20 @@
+const btnTab = document.querySelectorAll(".tab-btn");
+const about = document.querySelector(".about-main");
+const articles = document.querySelectorAll(".content");
+
+about.addEventListener("click", function (e) {
+  const id = e.target.dataset.id;
+  if (id) {
+    //remove active from other button
+    btnTab.forEach(function (btn) {
+      btn.classList.remove("active");
+      e.target.classList.add("active");
+    });
+    // hide other articles
+    articles.forEach(function (article) {
+      article.classList.remove("active");
+    });
+    const element = document.getElementById(id);
+    element.classList.add("active");
+  }
+});
